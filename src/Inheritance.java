edd3f9b48662d63e64a8d4950323e985a3d87d60
startupkenya <kkairo@gmail.com>
2015-06-30 12:16:04 +0300

    added php tags

--- src/Inheritance.java
+++ src/Inheritance.java
@@ -5,4 +5,9 @@ public class Inheritance extends Tuesday {
 		//calling a function in Tuesday class.
 		printSomething("Inherited");
 	}
+	
+	//this is a modification to test Git
+	public void justAFunction(){
+		
+	}
 }
